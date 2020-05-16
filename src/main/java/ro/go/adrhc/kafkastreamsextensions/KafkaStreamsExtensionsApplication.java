499c0729ac927bb92bdfab068774dd78315d99b3
adrhc <adrhc@outlook.com>
2020-05-16 16:45:50 +0300

    init

---
+++ src/main/java/ro/go/adrhc/kafkastreamsextensions/KafkaStreamsExtensionsApplication.java
@@ -0,0 +1,13 @@
+package ro.go.adrhc.kafkastreamsextensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsExtensionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsExtensionsApplication.class, args);
+	}
+
+}
